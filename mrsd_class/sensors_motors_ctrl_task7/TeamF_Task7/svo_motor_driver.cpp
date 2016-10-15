c40cc949c3383cc92196416d39be702f735617ad
Karthik Ramachandran <karthik@Karthiks-MacBook-Pro.local>
2016-10-14 21:10:16 -0400

    cleanup

--- mrsd_class/sensors_motors_ctrl_task7/TeamF_Task7/svo_motor_driver.cpp
+++ mrsd_class/sensors_motors_ctrl_task7/TeamF_Task7/svo_motor_driver.cpp
@@ -5,14 +5,14 @@ Servo svo;
 ServoState servoState;
 UltraSoundState ultraSoundState;
 
-void setupServo() {
-  // Attach servo
-  svo.attach(13);
-  
+// Setup
+void setupServoUltrasound() {
+  // Setup servo
+  svo.attach(13);  
   servoState.svo_pos = 0;  
   servoState.on = 1;  
   
-  // Setup Ultrasound
+  // Setup ultrasound
   ultraSoundState.MAX_DIST = 256;  
   ultraSoundState.MIN_DIST = 6;
   ultraSoundState.pin = A3;
@@ -21,43 +21,50 @@ void setupServo() {
   
 }
 
-void updateDistance() {
-  int sum = 0;
-  for (int i = 0; i < 5 ; i++)
-  {
-    int volt = analogRead(ultraSoundState.pin);
-    sum += volt;
-    delay(5);
-  }
-  ultraSoundState.dist = sum/5;
-}
-
+// Servo state update
 void updateServoState(int on) {
   servoState.on = on;
 }
 
+// Ultrasound state update
 void updateUSSensorState(int on) {
   ultraSoundState.on = on;
 }
 
+// Get distance from Ultrasound
+void updateDistance() {
+  int sum = 0;
+  int numSamples = 5;
+  for (int i = 0; i < numSamples ; i++) {
+    int volt = analogRead(ultraSoundState.pin);
+    sum += volt;
+    delay(5);
+  }
+  ultraSoundState.dist = (sum/numSamples) * 0.5;
+}
+
+// Drive servo to specified angle
 void driveServo(int angle){
   if (servoState.on) {
     int currAngle = svo.read();
     if (currAngle > angle) {
       for (int svo_pos = currAngle; svo_pos >= angle; svo_pos -= 1) { 
         svo.write(svo_pos);              
-        delay(20);
       }
     } else {
       for (int svo_pos = currAngle; svo_pos <= angle; svo_pos += 1) { 
         svo.write(svo_pos);              
-        delay(20);
       }
     }
+    delay(20);
     servoState.svo_pos = angle;
   }
 } 
 
+// Compute servo angle. 
+// Servo is set to 0 for less than 12 inches
+// Servo is set to a position determined as a linear function of distance between 12 and 48 inches
+// Servo is set to 180 beyond 48 inches.
 int computeAngle(int dist) {
   if (dist < 12) {
     return 0;
@@ -68,11 +75,10 @@ int computeAngle(int dist) {
   }
 }
 
+// Drive servo based on distance.
 void driveServo() {
-
   updateDistance();
   if ((servoState.on == 1) && (ultraSoundState.on == 1)) {
-    int angle = computeAngle(ultraSoundState.dist);
-    driveServo(angle);
+    driveServo(computeAngle(ultraSoundState.dist));
   }
 }
--- mrsd_class/sensors_motors_ctrl_task7/TeamF_Task7/svo_motor_driver.h
+++ mrsd_class/sensors_motors_ctrl_task7/TeamF_Task7/svo_motor_driver.h
@@ -3,6 +3,7 @@
 
 #include <Servo.h>
 
+// Ultra sound state
 struct UltraSoundState { 
   int MAX_DIST;
   int MIN_DIST;
@@ -11,17 +12,20 @@ struct UltraSoundState {
   int on;
 };
 
+// Servo state
 struct ServoState {
   int svo_pos;
   int on;
 }; 
 
+// Servo API functions
 void setupServo();
 void driveServo(int angle);
 void driveServo();
 void updateUSSensorState(int on);
 void updateServoState(int on);
 
+// Global state
 extern UltraSoundState ultraSoundState;
 extern ServoState servoState;
 
