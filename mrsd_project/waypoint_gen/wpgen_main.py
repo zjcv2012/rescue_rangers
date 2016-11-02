328d01ef8b1f2ae372fd15abb040377174dbf592
Karthik Ramachandran <karthik@Karthiks-MacBook-Pro.local>
2016-11-01 23:26:43 -0400

    remove reference from py

--- mrsd_project/waypoint_gen/wpgen_main.py
+++ mrsd_project/waypoint_gen/wpgen_main.py
@@ -37,12 +37,3 @@ def main():
 if __name__ == "__main__":
     main()
 
-# References
-# http://www.movable-type.co.uk/scripts/latlong.html
-# https://en.wikipedia.org/wiki/Decimal_degrees
-# http://stackoverflow.com/questions/1185408/converting-from-longitude-latitude-to-cartesian-coordinates
-# http://www.latlong.net/place/seattle-wa-usa-2655.html
-# http://www.gps-coordinates.net/gps-coordinates-converter
-# http://www.darrinward.com/lat-long/?id=2365962
-# http://www.gpsvisualizer.com/map_input?form=data
-# http://www.hamstermap.com/quickmap.php
